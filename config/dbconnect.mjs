37744a97a6880575e60e88af7d67e45022f2d755
jeunessesept <b.jiacinto@gmail.com>
2023-03-02 18:00:31 +0100

    correction comment

--- config/dbconnect.mjs
+++ config/dbconnect.mjs
@@ -3,7 +3,8 @@ import dotenv from "dotenv"// => package pour charger les variables d'environnem
 dotenv.config() //=> config pour pouvoir appeler les variables d'environnement
 
 
-export const dbConnect = mysql.createPool({  //=> instance de connection à mysql (elle est exportée pour pouvoir être utilisé ailleurs)
+export const dbConnect = mysql.createPool({  //=> instance de connection à mysql (elle est exportée pour pouvoir être utilisé par les Controllers)
+                                             // j'utilise createPool() plutot que createConnection() parce que cette dernière se coupe après 1 minute d'inactivité
     host: process.env.DB_HOST, // spécifie l'hôte de la database
     user: process.env.DB_USER, //spécifie l'utilisateur nécessaire pour se connecter à la database
     password: process.env.DB_PASSWORD, // spécifie le mot de passe pour se connecter à la database
