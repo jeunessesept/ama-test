fa30ebb518d171fd696fe74caf6736c90e1b0dde
jeunessesept <b.jiacinto@gmail.com>
2023-03-02 15:57:38 +0100

    heroku mysql config

--- config/dbconnect.mjs
+++ config/dbconnect.mjs
@@ -3,7 +3,7 @@ import dotenv from "dotenv"// => package pour charger les variables d'environnem
 dotenv.config() //=> config pour pouvoir appeler les variables d'environnement
 
 
-export const dbConnect = mysql.createConnection({  //=> instance de connection à mysql (elle est exportée pour pouvoir être utilisé ailleurs)
+export const dbConnect = mysql.createPool({  //=> instance de connection à mysql (elle est exportée pour pouvoir être utilisé ailleurs)
     host: process.env.DB_HOST, // spécifie l'hôte de la database
     user: process.env.DB_USER, //spécifie l'utilisateur nécessaire pour se connecter à la database
     password: process.env.DB_PASSWORD, // spécifie le mot de passe pour se connecter à la database
